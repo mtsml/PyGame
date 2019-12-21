7e0ab25daf6c15c7f05ff14e14888176100d113f
野上和希 <nokamikazuki@knmac001.local>
2019-12-21 20:48:10 +0900

    [Add] dead

--- cave.py
+++ cave.py
@@ -4,9 +4,12 @@ import pygame
 from pygame.locals import QUIT, Rect, KEYDOWN, K_SPACE, K_RETURN
 
 pygame.init()
-pygame.key.set_repeat(5,5)
-SURFACE = pygame.display.set_mode((1000,600))
+pygame.key.set_repeat(5, 5)
+SURFACE = pygame.display.set_mode((1000, 600))
 FPSCLOCK = pygame.time.Clock()
+ZANKI = 2
+dead_image = pygame.image.load("dead.png")
+
 
 def main():
     walls = 50
@@ -15,48 +18,50 @@ def main():
     score = 0
     slope = randint(5, 6)
     sysfont = pygame.font.SysFont(None, 36)
-    ship_image=pygame.image.load("ship.png")
-    bang_image=pygame.image.load("bang.png")
+    ship_image = pygame.image.load("ship.png")
+    bang_image = pygame.image.load("bang.png")
+    dead_image = pygame.image.load("dead.png")
     holes = []
     bg_color = [0, 255, 0]
     fps = 15
     inflate = 0
+    global ZANKI
 
     for xpos in range(walls):
         holes.append(Rect(xpos * 20, 100, 20, 400))
 
-    game_over=False
+    game_over = False
 
     while True:
-        is_space_down=False
+        is_space_down = False
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
             elif event.type == KEYDOWN:
                 if event.key == K_SPACE:
-                    is_space_down=True
+                    is_space_down = True
 
-        #自機を移動
+        # 自機を移動
         if not game_over:
             score += 10
-            if score >= 3000 and score < 6000 :
+            if score >= 3000 and score < 6000:
                 bg_color[0] = 0
                 bg_color[1] = 0
                 bg_color[2] = 255
                 fps = 20
                 inflate = -20
-            
+
             elif score >= 6000:
                 bg_color[0] = 255
                 bg_color[1] = 0
                 bg_color[2] = 0
-                inflate = 20                
+                inflate = 20
 
             velocity += -3 if is_space_down else 3
             ship_y += velocity
 
-            #洞窟をスクロール
+            # 洞窟をスクロール
             edge = holes[-1].copy()
             test = edge.move(0, slope)
             if test.top <= 0 or test.bottom >= 600:
@@ -67,26 +72,35 @@ def main():
             del holes[0]
             holes = [x.move(-20, 0) for x in holes]
 
-            #衝突?
+            # 衝突?
             if holes[0].top > ship_y or holes[0].bottom < ship_y + 30:
                 game_over = True
+                SURFACE.blit(dead_image, (50, 50))
         else:
+            # ゲームオーバーの場合main()を抜ける
             return
 
-        #描画
+        # 描画
         SURFACE.fill((bg_color[0], bg_color[1], bg_color[2]))
         for hole in holes:
             pygame.draw.rect(SURFACE, (0, 0, 0), hole)
 
         SURFACE.blit(ship_image, (0, ship_y))
-        score_image = sysfont.render("Score Is {}".format(score), True, (0, 0, 225))
+
+        score_image = sysfont.render(
+            "Score Is {}".format(score), True, (255, 255, 255))
         SURFACE.blit(score_image, (600, 20))
+
+        zanki_image = sysfont.render(
+            "Life : {}".format(ZANKI), True, (255, 255, 255))
+        SURFACE.blit(zanki_image, (50, 20))
 
         if game_over:
             SURFACE.blit(bang_image, (0, ship_y))
 
         pygame.display.update()
         FPSCLOCK.tick(fps)
+
 
 if __name__ == '__main__':
     main()
@@ -96,6 +110,13 @@ if __name__ == '__main__':
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+            elif ZANKI <= 0:
+                SURFACE.blit(dead_image, (50, 50))
+                pygame.display.update()
+                if event.type == KEYDOWN and event.key == K_RETURN:
+                    pygame.quit()
+                    sys.exit()
             elif event.type == KEYDOWN:
-                if event.key == K_RETURN:
+                if event.key == K_SPACE:
+                    ZANKI -= 1
                     main()
